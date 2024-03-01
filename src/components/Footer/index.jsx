27e4e8ff28bdf6b822ddde54f8927bda46d77f7e
afontana405 <alex13579@icloud.com>
2024-02-29 23:39:58 -0600

    built footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,29 @@
+function Footer() {
+    
+    const icons = [
+        {
+            name: "fab fa-github",
+            link: "https://github.com/afontana405"
+        },
+        {
+            name: "fab fa-linkedin",
+            link: "https://www.linkedin.com/in/alex-fontana-58bb76288/"
+        },
+        {
+            name: "fab fa-stack-overflow",
+            link: "https://stackoverflow.com/"
+        }
+    ]
+
+    return (
+        <footer className="">
+            {icons.map(icon => 
+                (
+                    <a href={icon.link} key={icon.name} target="_blank"><i className={icon.name}></i></a>
+                )
+            )}
+        </footer>
+    )
+}
+
+export default Footer;
