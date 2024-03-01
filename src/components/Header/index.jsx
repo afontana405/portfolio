6e6d6e20c8820516dd5770c2c99b5864e04ab5a7
afontana405 <alex13579@icloud.com>
2024-03-01 13:53:44 -0600

    built header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import coverImage from '../../assets';
+
+function Header() {
+
+    return (
+        <header className="">
+            <h1>Fontana</h1>
+            <img src={coverImage} alt=""></img>
+        </header>
+    );
+}
+
+export default Header;
