a14b9b8db567234c56b7bb308168679153c9533b
afontana405 <alex13579@icloud.com>
2024-03-02 22:33:04 -0600

    built nav bar

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,21 @@
+function Nav({ currentPage }) {
+    const pages = ['About', 'Portfolio', 'Contact', 'Resume'];
+
+    return (
+        <nav>
+            <ul className="">
+                {pages.map((page) => (
+                    <li
+                        className=""
+                        key={page}
+                    >
+                        <Link to={`/${page}`}>{page}</Link>
+                    </li>
+                ))
+                }
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
