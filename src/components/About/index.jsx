d9c74dc2f91e56a688a3ae568c0a6aeaff6c4642
afontana405 <alex13579@icloud.com>
2024-03-03 21:39:56 -0600

    built about content with temp text

---
+++ src/components/About/index.jsx
@@ -0,0 +1,17 @@
+function About() {
+    return(
+        <section className="">
+            <div className="">
+                <i className="fas fa-user-circle" style={{ fontSize: "96px"}}></i>
+            </div>
+            <p>
+                filler text isnasd fdklfadj dsaafdfa dfalkdsaj dafkjakfljddS DSAd sasad SDA SDAFDSASA dfdadf  dadfdadfas ddadfdf
+            </p>
+            <p>
+                dafsfdsfdf dfsafdafsddf afdsfdsafdsa dsfafafddsaf dfafdsdsadfdsadfd fdsafdfasdfdf ddfasf
+            </p>
+        </section>
+    )
+}
+
+export default About;
