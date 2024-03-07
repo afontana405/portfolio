3632ff028c9b336af6aaa852b74fe89064f168fc
afontana405 <alex13579@icloud.com>
2024-03-06 21:46:22 -0600

    added two projects to portfolio page

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+
 function Portfolio() {
+
+    const [projects] = useState([
+        {
+            name: 'FlashCard App',
+            description: 'MERN Stack Flash Card Application',
+            link: 'https://github.com/',
+            repo: 'https://github.com/'
+        },
+        {
+            name: 'Resturant Reservation App',
+            descrpiton: 'Mern Stack Reservation Application',
+            link: 'https://github.com/',
+            repo: 'https://github.com/',
+        }
+    ])
     return(
         <p>Portfolio</p>
     )
