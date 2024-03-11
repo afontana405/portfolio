fd9884c20e574e9e444648c899c85dd35a7c223b
afontana405 <alex13579@icloud.com>
2024-03-11 16:49:01 -0500

    added e commerce pic and exported to project component

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,3 @@
 export { default as flashCard } from './projects/flashCard.png';
 export { default as resturantReservation } from './projects/resturantReservation.jpg';
-
+export { default as ECommerceBackend} from './projects/EcommerceBackend.jfif';
