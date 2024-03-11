4dfdb0d3db0bb23e37765ab039b21a77114d0f8f
afontana405 <alex13579@icloud.com>
2024-03-11 18:25:47 -0500

    added links to flashcard and reservation projects

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -7,18 +7,18 @@ function Portfolio() {
         {
             name: 'flashCard',
             description: 'MERN Stack Flash Card Application',
-            link: 'https://github.com/',
-            repo: 'https://github.com/'
+            link: 'https://flash-orama.onrender.com',
+            repo: 'https://github.com/afontana405/flash-orama'
         },
         {
             name: 'resturantReservation',
-            description: 'Mern Stack Reservation Application',
-            link: 'https://github.com/',
-            repo: 'https://github.com/'
+            description: 'MERN Stack Reservation Application',
+            link: 'https://feastfinder-29c2a8dddaa0.herokuapp.com',
+            repo: 'https://github.com/alamm01/FeastFinder'
         },
         {
             name: 'ECommerceBackend',
-            description: 'E-Commerce Backend Application',
+            description: 'MySQL E-Commerce Backend Application',
             link: 'https://github.com/',
             repo: 'https://github.com/',
         }
